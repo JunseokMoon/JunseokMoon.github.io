577303970ae9217f9d1ce8e82064a72ea2f5e991
JunseokMoon <108853740+JunseokMoon@users.noreply.github.com>
2026-01-18 15:01:01 +0000

    Deploying to gh-pages from @ JunseokMoon/JunseokMoon.github.io@7b4d1a02b972660ee033b8f4ebc14ffed6a47e7f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-research",
+          title: "Research",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
         },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
@@ -36,13 +43,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "post-machine-learning-driven-materials-discovery",
         
